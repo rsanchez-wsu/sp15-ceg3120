12a893b52ab71b155b2076622b0fbe4e1b67831f
Roberto C. Sánchez <roberto.sanchez@wright.edu>
2015-01-26 19:28:47 -0500

    Apply GPL-3 license, as discussed in class.

--- src/team5/Client/ClientJList.java
+++ src/team5/Client/ClientJList.java
@@ -1,9 +1,21 @@
-package team5.Client;
-
 /*
- * License tbd
- * @author hitchens6
+ *  Copyright (C) <year>  <name of author>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
+package team5.Client;
 
 import java.awt.*;
 import java.util.Vector;
@@ -11,6 +23,10 @@ import java.util.Vector;
 import javax.swing.*;
 
 import team5.PlayerObject.Player;
+
+/**
+ * @author hitchens6
+ */
 
 public class ClientJList extends JPanel {
 	/**
