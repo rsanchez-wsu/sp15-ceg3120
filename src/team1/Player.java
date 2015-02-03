f45f98cccba5167d9d30ded95729d726f9de0df2
KWintermute <wintermute2546@gmail.com>
2015-02-03 18:19:09 -0500

    Mocked up Player class for group 1.   Added Health and Position fields, getter methods, and Attack, Pass, and Move, dummy methods.

---
+++ src/team1/Player.java
@@ -0,0 +1,27 @@
+package team1;
+
+public class Player {
+	
+	private int health;
+	private int[] position;
+	
+	public void attack(){
+		
+	}
+
+	public void move(){
+		
+	}
+	
+	public void pass(){
+		
+	}
+	
+	public int getHealth(){
+		return health;
+	}
+	public int[] getPosition(){
+		return position;
+	}
+	
+}
