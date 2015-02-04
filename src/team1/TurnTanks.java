163a8d93cd04d3842c15cf08d3fa7b07e4359387
w040kjw (Kyle Wood) <kwood07@woh.rr.com>
2015-02-03 20:05:51 -0500

    Uplodaing edits at end of meeting (2/3)
    @w040kjw
    @kristenschwaiger
    @mariehucke
    @KWintermute

--- src/team1/TurnTanks.java
+++ src/team1/TurnTanks.java
@@ -1,3 +1,21 @@
+/*
+ *  Copyright (C) <2015>  Marie Hucke, Kristen Schwaiger, Kyle Wintermute, Kyle Wood
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package team1;
 
 import java.awt.*;
@@ -8,17 +26,24 @@ import javax.swing.*;
 
 public class TurnTanks extends JPanel implements ActionListener {
 	
+	private JList PlayerList;
+	
 	public TurnTanks() {
 		
-		JPanel PlayerList = new JPanel();
-		GridBagLayout PlayerListLayout = new GridBagLayout();
+		
+		Player[] Players = new Player[8];
+		
+		PlayerList = new JList(Players);
+		PlayerList.setVisibleRowCount(-1);
+		
+		//JPane PLPane = new JPane(PlayerList);
+		
+		JPanel PlayerPanel = new JPanel();
+		GridBagLayout PlayerPanelLayout = new GridBagLayout();
 		GridBagConstraints GBConstraint = new GridBagConstraints();
 		
-		PlayerList.setLayout(PlayerListLayout);
+		PlayerPanel.setLayout(PlayerPanelLayout);
 		
-		JLabel title = new JLabel("TURN TANKS");
-		
-		PlayerList.add(title);
 	}
 	
 	@Override
