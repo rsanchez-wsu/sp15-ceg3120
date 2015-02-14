c1780d97d4f3b8af4c4ecac8b6f86079078d77eb
hitchens6 <hitchens6.@wright.edu>
2015-02-13 22:56:54 -0500

    @hitchens6 added header to sockets

--- src/team5/client/socket/SocketClient.java
+++ src/team5/client/socket/SocketClient.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (C) <2015>  <Joshua Hitchens, Lori Simmons, Ryan Lane, Tyler Parker>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package team5.client.socket;
 
 import java.io.*;
@@ -40,4 +57,10 @@ public class SocketClient {
         
         System.out.println("Exiting");
     }
+	public static String getServerName() {
+		return serverName;
+	}
+	public static void setServerName(String serverName) {
+		SocketClient.serverName = serverName;
+	}
 }
