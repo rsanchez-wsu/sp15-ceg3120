fc20599220a828e6acdde0498cace108c57657b7
marie.hucke <marie.hucke@LL-MHucke>
2015-02-23 09:06:32 -0500

    Further development of game map. Added paint method to add tiles to game map. @w040kjw @kristenschwaiger @mariehucke @KWintermute

--- src/team1/Game_Map.java
+++ src/team1/Game_Map.java
@@ -17,15 +17,43 @@
  */
 package team1;
 
-import java.awt.Color;
+import java.awt.*;
 
 import javax.swing.*;
 
-	public class Game_Map {
-		
 
-	private void createNewMap(){
-		JPanel gameMap = new JPanel();
-		gameMap.setBackground(Color.DARK_GRAY);
+
+	public class Game_Map extends JPanel{
+
+	static JPanel gameMap = new JPanel(); //create the game map jPanel
+		
+	int rows = 64; //number of tiles down
+	int cols = 64; //number of tiles across
+	
+	
+	public void paintComponent(Graphics g){ //painting the tiles onto the panel
+		super.paintComponent(g);
+		g.clearRect(0, 0, getWidth(), getHeight());
+		int tileSize = 20; //pixels for each tile
+		int width = cols * tileSize; //width of the jPanel
+		int height = rows * tileSize; //height of the jPanel
+		int panelWidth = getWidth()/cols; //setting panel width
+		int panelHeight = getHeight()/rows; //seting panel height
+		
+		Dimension setPreferredSize = (new Dimension(width, height)); //set the preferred size of the jPanel
+		
+		for (int i=0; i<rows; i++){ //loop to paint rows of tiles
+			for(int j=0; j<cols; j++){ //loop to paint cols of tiles
+				int w = i*panelWidth; 
+				int h = j*panelHeight;
+				g.setColor(Color.DARK_GRAY); //color each tile dark gray
+				g.fillRect(w,h,panelWidth,panelHeight); //fill the jPanel with tiles
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args){
+		gameMap.setVisible(true); //set the game map to visible
 	}
 }
